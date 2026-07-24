4dfac09032613a3e2065ec81e591dfefc2a96a4b
NusrathChowdhury <fnchyyyy@gmail.com>
2026-07-24 22:57:02 +0600

    parameter

---
+++ parameter.js
@@ -0,0 +1,23 @@
+
+
+// function sayHello (){
+//     console.log('Hello!,FNC')
+// }
+
+// sayHello()
+
+function add(num1,num2){
+    console.log('u have provided',num1,num2)
+    const sum= num1+num2;
+    console.log('total of the provided number',sum);
+
+}
+add(25,8)
+
+
+function all(a,b,c,d){
+    total=a+b/c*d;
+    console.log('total result',total)
+}
+
+all(55,9,2,2)
