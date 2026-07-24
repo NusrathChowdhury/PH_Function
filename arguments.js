fef1432df9044caada7c8766f860af59f58c4420
NusrathChowdhury <fnchyyyy@gmail.com>
2026-07-25 01:45:00 +0600

    Your commit message

---
+++ arguments.js
@@ -0,0 +1,13 @@
+
+
+
+function multiply(a,b){
+    const result=a*b;
+    console.log(result)
+
+}
+multiply(3,5)
+
+
+
+
---
+++ return.js
@@ -0,0 +1,21 @@
+
+
+function cutHalf(number){
+    const half =number/2;
+    return half;
+
+}
+
+const result=cutHalf(100);
+console.log(result)
+
+const number2=cutHalf(80);
+function add(num1,numb2){
+    const sum=num1+numb2;
+    return sum;
+
+}
+const total =add(12, 16);
+console.log(total);
+const total2= add(result,number2);
+console.log('total is', total)
